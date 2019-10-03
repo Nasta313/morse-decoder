c518107d99e05f097cd93b05b543d4f70746e91b
Nasta Vlasovets <vlasovec.nasta313@gmail.com>
2019-10-03 16:42:43 +0300

    feat: implementation of the decode function

--- src/index.js
+++ src/index.js
@@ -38,7 +38,21 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+
+    let stack = [];
+
+    for (let i = 0; i < expr.length; i+=10) {               
+        let symbol = expr.slice(i, i+10);   
+        
+        if ( symbol === "**********" ) {
+            stack.push(" ");
+        } else {
+            let code = symbol.replace(/11/g, "-").replace(/0/g, "").replace(/1/g, "\.");
+            stack.push(MORSE_TABLE[code]);     
+        }   
+    }
+       
+    return stack.join('');
 }
 
 module.exports = {
